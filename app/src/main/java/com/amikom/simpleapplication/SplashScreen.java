37d98900d052bfb13c751462392e1f3cdffac274
maylani <maylaniintan7@gmail.com>
2018-10-05 07:49:17 +0700

    project

---
+++ app/src/main/java/com/amikom/simpleapplication/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.amikom.simpleapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amikom.simpleapplication.util.PreferencesHelper;
+
+public class SplashScreen extends AppCompatActivity {
+    PreferencesHelper instance;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        instance = PreferencesHelper.getInstance(getApplicationContext());
+        int splashInterval = 10;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!instance.isLogin()) {
+                    startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                } else {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }
+            }
+        });
+    }
+}
